d04745d4ed931bb9b60538b8abd5002c1f5a9e7c
Hager Hu <hager.hu@gmail.com>
2014-01-24 23:44:30 +0800

    更新组织结构，支持Pods形式

---
+++ Classes/MQTTSDK.h
@@ -0,0 +1,19 @@
+//
+//  MQTTSDK.h
+//  MQTTSDK
+//
+//  Created by Hager Hu on 1/24/14.
+//  Copyright (c) 2014 Travel Studio. All rights reserved.
+//
+
+#ifndef MQTTSDK_MQTTSDK_h
+#define MQTTSDK_MQTTSDK_h
+
+#import "MQTTDecoder.h"
+#import "MQTTEncoder.h"
+
+#import "MQTTMessage.h"
+#import "MQTTSession.h"
+#import "MQttTxFlow.h"
+
+#endif
